10870b0bcfbc7799aa7c881b56be4535f147a5b7
Kanchi0914 <dentipass205>
2019-11-12 03:19:45 +0900

    Init

---
+++ generator/myTest.py
@@ -0,0 +1,6 @@
+from generator.names import Names
+
+if __name__ == '__main__':
+    names = Names()
+    names.get_names()
+
---
+++ generator/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import TestModel
+from django.views.generic import View
+from django.http.response import HttpResponse
+from django.http.response import JsonResponse
+
+from keras.models import Model, load_model, save_model
+from .keras_name_generator import dictchars_maker, model_setter, name_generator,\
+    name_processor, onehot_vec_maker, sequences_maker, csv_loader
+from django.conf import settings
+
+class Names():
+    def __init__(self):
+        self.load()
+
+    def load(self):
+        base2 = settings.BASE_DIR + r"\generator\keras_name_generator" + "\\"
+
+        self.model = load_model(base2 + r"models\model.h5")
+        self.model.load_weights(base2 + r"weights\weights7.h5")
+
+        self.datas = csv_loader.load_csv(base2 + r"csv\en_jp_names.csv")
+        self.names = [data[1] for data in self.datas]
+        self.names, self.chars = name_processor.process_names(self.names)
+
+        seqlen = 4
+        self.dictchars = dictchars_maker.get_dictchars(self.names, seqlen)
+        # with keras.backend.get_session().graph.as_default():
+
+    def get_name(self):
+        list = self.get_names()
+        text = ""
+        for t in list:
+            text += t + "\n"
+        return text
+
+    def get_names(self):
+        list = []
+        for i in range(10):
+            list.append(name_generator.generate_random_name
+                        (self.model, chars=self.chars, dictchars=self.dictchars))
+        return list
+        # with keras.backend.get_session().as_default():  #追加
+
+
+generator = Names()
+generator.get_names()
+
+# Create your views here.
+class IndexView(generic.ListView):
+    model = TestModel
+    template_name = 'sample/index.html'
+
+def generator_template(request):
+    return render(request, "generator/index.html")
+
+def exec_ajax(request):
+    # names2 = Names()
+    names = generator.get_names()
+
+    if request.method == 'GET':
+        param1 = request.GET.get("param1")
+        return JsonResponse(names, safe=False)
+
+
+        #return HttpResponse({"value1":"testoooooooooooooo"})
+    #
+    # if request.method == 'POST':  # POSTの処理
+    #     data1 = request.POST.get("input_data")  # POSTで渡された値
+    #     return HttpResponse("SUCCESS")
+
+    # if request.POST.get('click', False):
+    #     return HttpResponse("SUCCESS")
+    # if request.method == "POST":
+    #     return HttpResponse("SUCCESS")
+    # else:
+    #     return HttpResponse("SUCCESS??????")
+
